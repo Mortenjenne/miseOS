0b1703433b8245a7a11dd26f17e62fa2c13fd347
Morten Jensen <mo_ten@hotmail.com>
2026-02-07 13:19:34 +0100

    Added base User structure

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,5 +1,31 @@
 package app.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@ToString
+@EqualsAndHashCode
 public class User
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String hashedPassword;
+
+    private Role role;
+
+    private Station station;
 }
