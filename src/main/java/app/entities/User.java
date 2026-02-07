ae9de73da87e94e53d36d7a06bf31bc7b4b66018
Morten Jensen <mo_ten@hotmail.com>
2026-02-07 16:28:02 +0100

    Implemented integration test on CRUD methods in UserDAOImpl

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+@Builder(toBuilder = true)
 @Getter
 @ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
--- src/test/java/app/daos/impl/UserDAOImplTest.java
+++ src/test/java/app/daos/impl/UserDAOImplTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.*;
 
 import javax.swing.text.html.parser.Entity;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +20,7 @@ class UserDAOImplTest
 {
     private static EntityManagerFactory emf;
     private static UserDAO userDAO;
+    private static User user;
 
     @BeforeAll
     static void setup()
@@ -29,14 +31,15 @@ class UserDAOImplTest
     @BeforeEach
     void init()
     {
-        userDAO = new UserDAOImpl(emf);
-    }
+        try (EntityManager em = emf.createEntityManager())
+        {
+            em.getTransaction().begin();
+            em.createQuery("DELETE FROM User").executeUpdate();
+            em.getTransaction().commit();
+        }
 
-    @DisplayName("Test create and findByID")
-    @Test
-    void createAndFind()
-    {
-        User user = User.builder()
+        userDAO = new UserDAOImpl(emf);
+        user = User.builder()
             .firstName("Mads")
             .lastName("Kok")
             .email("mads@miseos.dk")
@@ -45,38 +48,102 @@ class UserDAOImplTest
             .station(Station.HOT)
             .build();
 
-        userDAO.create(user);
 
-        assertTrue(user.getId() > 0);
+    }
+
+    @DisplayName("Test create and findByID")
+    @Test
+    void createAndFind()
+    {
+        User userFromDB = userDAO.create(user);
+
+        assertTrue(userFromDB.getId() > 0);
 
         Optional<User> found = userDAO.findById(user.getId());
         assertTrue(found.isPresent());
         assertEquals("Mads", found.get().getFirstName());
+        assertEquals("Kok", found.get().getLastName());
+        assertTrue(found.get().getStation().equals(Station.HOT));
     }
 
-    @Test
-    void findById()
-    {
-    }
-
+    @DisplayName("Test find by email")
     @Test
     void findByEmail()
     {
+        String email = user.getEmail();
+        userDAO.create(user);
+        Optional<User> found = userDAO.findByEmail(email);
+        assertTrue(found.isPresent());
+        assertEquals(email, found.get().getEmail());
+    }
+
+    @DisplayName("Test find by email, where email dosen't exit in db")
+    @Test
+    void findByEmailNotPresent()
+    {
+        String email = "test@notfound.dk";
+        userDAO.create(user);
+        Optional<User> found = userDAO.findByEmail(email);
+        assertFalse(found.isPresent());
     }
 
     @Test
+    @DisplayName("Test find all users")
     void findAll()
     {
+        userDAO.create(user);
+        User user2 = User.builder()
+            .firstName("Jane")
+            .lastName("Doe")
+            .email("benny@miseos.dk")
+            .hashedPassword("5678")
+            .role(Role.LINE_COOK)
+            .station(Station.COLD)
+            .build();
+        userDAO.create(user2);
+
+        List<User> users = userDAO.findAll();
+
+        assertEquals(2, users.size());
+        boolean hasMads = users.stream().anyMatch(u -> u.getFirstName().equals("Mads"));
+        boolean hasBenny = users.stream().anyMatch(u -> u.getFirstName().equals("Jane"));
+
+        assertTrue(hasMads);
+        assertTrue(hasBenny);
     }
 
+    @DisplayName("Test updating user")
     @Test
     void update()
     {
+        User createdUser = userDAO.create(user);
+        User userToUpdate = createdUser.toBuilder()
+            .firstName("John")
+            .lastName("Smith")
+            .role(Role.CHEF_DE_PARTIE)
+            .station(Station.COLD)
+            .build();
+
+        User updatedUser = userDAO.update(userToUpdate);
+
+        assertEquals("John", updatedUser.getFirstName());
+        assertEquals(Role.CHEF_DE_PARTIE, updatedUser.getRole());
+        assertEquals(Station.COLD, updatedUser.getStation());
+        assertEquals(createdUser.getId(), updatedUser.getId());
+
     }
 
     @Test
+    @DisplayName("Test delete user")
     void delete()
     {
+        userDAO.create(user);
+
+        boolean deleted = userDAO.delete(user.getId());
+        assertTrue(deleted);
+
+        Optional<User> found = userDAO.findById(user.getId());
+        assertFalse(found.isPresent());
     }
 
     @AfterAll
