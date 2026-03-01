9b16f4becf3a9e36449e85d4a82e41d98fd15211
Morten Jensen <mo_ten@hotmail.com>
2026-03-01 13:17:26 +0100

    Added DishSuggestionMapper for converting DishSuggestion -> DishsuggestionDTO

--- src/main/java/app/mappers/DishSuggestionMapper.java
+++ src/main/java/app/mappers/DishSuggestionMapper.java
@@ -1,4 +1,36 @@
 package app.mappers;
 
-public class DishSuggestionMapper {
+import app.dtos.dishsuggestion.DishSuggestionDTO;
+import app.persistence.entities.Allergen;
+import app.persistence.entities.DishSuggestion;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DishSuggestionMapper
+{
+
+    private DishSuggestionMapper() {}
+
+    public static DishSuggestionDTO toDTO(DishSuggestion suggestion)
+    {
+        Set<String> allergenNames = suggestion.getAllergens().stream()
+            .map(Allergen::getName)
+            .collect(Collectors.toSet());
+
+        String chefName = suggestion.getCreatedBy().getFirstName() + " " + suggestion.getCreatedBy().getLastName();
+
+        return new DishSuggestionDTO(
+            suggestion.getId(),
+            suggestion.getNameDA(),
+            suggestion.getDescriptionDA(),
+            suggestion.getDishStatus(),
+            suggestion.getFeedback(),
+            suggestion.getStation().getStationName(),
+            chefName,
+            allergenNames,
+            suggestion.getTargetWeek(),
+            suggestion.getTargetYear()
+        );
+    }
 }
