0c653bcbcb8c889acf57a6e6e750166624ac2efe
Morten Jensen <mo_ten@hotmail.com>
2026-03-01 13:31:46 +0100

    Added AllergenMapper for converting Allergen -> AllergenDTO

---
+++ src/main/java/app/mappers/AllergenMapper.java
@@ -0,0 +1,19 @@
+package app.mappers;
+
+import app.dtos.allergen.AllergenDTO;
+import app.persistence.entities.Allergen;
+
+public class AllergenMapper
+{
+    private AllergenMapper() {}
+
+    public AllergenDTO toDTO(Allergen allergen)
+    {
+        return new AllergenDTO(
+            allergen.getId(),
+            allergen.getName(),
+            allergen.getDescription(),
+            allergen.getDisplayNumber()
+        );
+    }
+}
