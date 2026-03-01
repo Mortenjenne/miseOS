8376ef5e0fe2b99f3f96e003a1a58d9eef014563
Morten Jensen <mo_ten@hotmail.com>
2026-03-01 13:13:39 +0100

    Added IngredientMapper for converting IngredientRequest -> IngredienRequestDTO

---
+++ src/main/java/app/mappers/IngredientRequestMapper.java
@@ -0,0 +1,28 @@
+package app.mappers;
+
+import app.dtos.ingredient.IngredientRequestDTO;
+import app.persistence.entities.IngredientRequest;
+
+public class IngredientRequestMapper
+{
+    private IngredientRequestMapper() {}
+
+    public static IngredientRequestDTO toDTO(IngredientRequest request)
+    {
+        return new IngredientRequestDTO(
+            request.getId(),
+            request.getName(),
+            request.getQuantity(),
+            request.getUnit(),
+            request.getPreferredSupplier(),
+            request.getNote(),
+            request.getRequestStatus(),
+            request.getRequestType(),
+            request.getDeliveryDate(),
+            request.getCreatedAt(),
+            request.getReviewedAt(),
+            request.getCreatedBy().getId(),
+            request.getDishSuggestion() != null ? request.getDishSuggestion().getId() : null
+        );
+    }
+}
