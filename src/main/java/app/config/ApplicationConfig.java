a15f98429a3bcffbd7f1a0a5fa2f76a87d185fae
Morten Jensen <mo_ten@hotmail.com>
2026-03-06 09:17:29 +0100

    refactor(config): cleaned ad hoc test in application config to make ready for javalin integration

--- src/main/java/app/config/ApplicationConfig.java
+++ src/main/java/app/config/ApplicationConfig.java
@@ -24,88 +24,9 @@ public class ApplicationConfig
 {
     public static void start()
     {
-        Properties properties = new Properties();
-
-        try (InputStream input = ApplicationConfig.class
-                     .getClassLoader()
-                     .getResourceAsStream("config.properties")) {
-
-            properties.load(input);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
-        HttpClient client = HttpClient.newHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        String deeplUrl = properties.getProperty("DEEPL_URL");
-        String geminiUrl = properties.getProperty("GEMINI_URL");
-        String openMeteoUrl = properties.getProperty("OPEN_METEO_URL");
-        String deepLApiKey = System.getenv("DEEPL_APIKEY");
-        String geminiApiKey = System.getenv("GEMINI_API_KEY");
-
-        ITranslationClient translationService = new DeepLTranslationClient(client, objectMapper, deeplUrl, deepLApiKey);
-        IAiClient aiClient = new GeminiClient(client, objectMapper, geminiApiKey, geminiUrl);
-        IAiService aiService = new AiService(objectMapper, aiClient);
-        WeatherClient weatherClient = new WeatherClient(client, objectMapper, openMeteoUrl);
-
-
-        IDishTranslationService dishTranslationService = new DishTranslationService(translationService);
-        WeatherForecastDTO weatherForecastDTO = weatherClient.getWeatherForecast();
-        String forecast = WeatherForecastBuilder.getWeatherForecast(weatherForecastDTO);
-
-        List<AiDishSuggestionDTO> dishSuggestionDTOs = aiService.getAiDishSuggestion(forecast, "Sandwich station");
-        dishSuggestionDTOs.forEach(System.out::println);
-
-        List<String> ingredientsToNormalize = List.of(
-            "onions",
-            "løg",
-            "red onion",
-            "rødløg",
-            "hvidløg",
-            "garlic",
-            "hvidløch",
-            "potatoes",
-            "nye kartofler",
-            "carots",
-            "gulerødder",
-            "gulerod",
-            "piskefløde",
-            "heavy cream",
-            "cream",
-            "smør",
-            "unsalted butter",
-            "butter",
-            "salt",
-            "havsalt",
-            "pepper",
-            "sort peber",
-            "peber",
-            "shallots",
-            "skalotteløg",
-            "skalotte løg",
-            "spring onions",
-            "forårsløg",
-            "chives",
-            "purløg",
-            "parsley",
-            "persille",
-            "lemon",
-            "citron",
-            "lime juice",
-            "limes",
-            "olive oil",
-            "olivenolie",
-            "extra virgin olive oil"
-        );
 
 
 
-        Map<String, String> result = aiService.normalizeIngredientList(ingredientsToNormalize, "da");
-
-        result.forEach((k, v) -> System.out.println("Key: " + k + " Value " + v));
 
 
     }
--- src/main/java/app/config/DIContainer.java
+++ src/main/java/app/config/DIContainer.java
@@ -1,19 +1,73 @@
 package app.config;
 
+import app.dtos.weather.WeatherForecastDTO;
+import app.integrations.ai.GeminiClient;
+import app.integrations.ai.IAiClient;
+import app.integrations.translation.DeepLTranslationClient;
+import app.integrations.translation.ITranslationClient;
+import app.integrations.weather.WeatherClient;
 import app.persistence.daos.impl.AllergenDAO;
 import app.persistence.daos.impl.DishDAO;
 import app.persistence.daos.interfaces.IAllergenDAO;
 import app.persistence.daos.interfaces.IDishDAO;
 import app.persistence.daos.interfaces.IDishSuggestionDAO;
+import app.services.IAiService;
+import app.services.IDishTranslationService;
+import app.services.impl.AiService;
+import app.services.impl.DishTranslationService;
+import app.utils.WeatherForecastBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManagerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.http.HttpClient;
+import java.util.Properties;
 
 public class DIContainer
 {
+    Properties properties = new Properties();
+
+        try (
+    InputStream input = ApplicationConfig.class
+        .getClassLoader()
+        .getResourceAsStream("config.properties")) {
+
+    properties.load(input);
+
+} catch (
+    IOException e) {
+    throw new RuntimeException(e);
+}
+
     private static final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
 
     private static IAllergenDAO allergenDAO;
     private static IDishDAO dishDAO;
     private static IDishSuggestionDAO dishSuggestionDAO;
+
+
+
+    HttpClient client = HttpClient.newHttpClient();
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    String deeplUrl = properties.getProperty("DEEPL_URL");
+    String geminiUrl = properties.getProperty("GEMINI_URL");
+    String openMeteoUrl = properties.getProperty("OPEN_METEO_URL");
+    String deepLApiKey = System.getenv("DEEPL_APIKEY");
+    String geminiApiKey = System.getenv("GEMINI_API_KEY");
+
+    ITranslationClient translationService = new DeepLTranslationClient(client, objectMapper, deeplUrl, deepLApiKey);
+    IAiClient aiClient = new GeminiClient(client, objectMapper, geminiApiKey, geminiUrl);
+    IAiService aiService = new AiService(objectMapper, aiClient);
+    WeatherClient weatherClient = new WeatherClient(client, objectMapper, openMeteoUrl);
+
+
+    IDishTranslationService dishTranslationService = new DishTranslationService(translationService);
+    WeatherForecastDTO weatherForecastDTO = weatherClient.getWeatherForecast();
+    String forecast = WeatherForecastBuilder.getWeatherForecast(weatherForecastDTO);
 
 
 
