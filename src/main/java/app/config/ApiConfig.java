354af8ec5ee93a94227e526b2860015292398f74
Morten Jensen <mo_ten@hotmail.com>
2026-03-24 20:04:38 +0100

    Removed urls from config.properties causing problems in workflow

--- src/main/java/app/config/ApiConfig.java
+++ src/main/java/app/config/ApiConfig.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 @Getter
 public class ApiConfig
 {
+    private static final String DEEPL_URL = "https://api-free.deepl.com/v2/translate";
+    private static final String GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview";
+    private static final String OPEN_METEO_URL = "https://api.open-meteo.com/v1/forecast?latitude=55.6759&longitude=12.5655&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,weathercode&timezone=Europe/Copenhagen&forecast_days=7";
     private final String deepLUrl;
     private final String deepLApiKey;
     private final String geminiUrl;
@@ -16,13 +19,13 @@ public class ApiConfig
 
     public ApiConfig()
     {
-        this.deepLUrl = AppProperties.get("DEEPL_URL");
+        this.deepLUrl = DEEPL_URL;
         this.deepLApiKey = System.getenv("DEEPL_APIKEY");
-        this.geminiUrl = AppProperties.get("GEMINI_URL");
+        this.geminiUrl = GEMINI_URL;
         this.geminiApiKey = System.getenv("GEMINI_API_KEY");
-        this.openMeteoUrl = AppProperties.get("OPEN_METEO_URL");
-        this.issuer = AppProperties.get("ISSUER");
+        this.openMeteoUrl = OPEN_METEO_URL;
+        this.issuer = System.getenv("ISSUER");
         this.secretKey = System.getenv("SECRET_KEY");
-        this.expirationMs = Long.parseLong(AppProperties.get("EXPIRATION_MS"));
+        this.expirationMs = Long.parseLong(System.getenv("TOKEN_EXPIRE_TIME"));
     }
 }
